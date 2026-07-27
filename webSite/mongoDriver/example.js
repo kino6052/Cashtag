efa2f5d9e2841c3cb1db6bdbd6c66b9842822baa
kino6052 <kirill.novik.csci@gmail.com>
2026-07-27 10:31:12 +0800

    Restructure

--- webSite/mongoDriver/example.js
+++
@@ -1,3 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-var client = new MongoClient(new Server('localhost', 27017,  { poolSize: 5}),
-                             { retryMiliSeconds: 500 });
